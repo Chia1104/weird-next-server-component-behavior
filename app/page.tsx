310f00966ab104eb2a1eef238bc9170742d5359e
Chia1104 <yuyuchia7423@gmail.com>
2026-01-02 05:08:13 +0800

    key

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,12 @@ import { ClientCounter } from "./client-counter";
 import { ServerCounter } from "./server-counter";
 import { Suspense } from "react";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+export default async function Home({ searchParams }: { searchParams: Promise<{ [key: string]: string }> }) {
   const { count } = await searchParams;
   return (
     <CounterAction>
       <ClientCounter />
-      <Suspense fallback={<div>Loading server counter...</div>}>
+      <Suspense fallback={<div>Loading server counter...</div>} key={count}>
         <ServerCounter count={Number(count) || 0} />
       </Suspense>
     </CounterAction>
